ce44ff42122eefa6a402ca335e78bfffeb18af26
Panchammmm <sardarpancham510510@gmail.com>
2024-03-23 02:11:14 +0530

    delete-button & function-added

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import ToDoItem from "./ToDoItem";
 function App() {
     const [inputText, setInputText] = useState("");
     const [items, setItems] = useState([]);
-
+    
     function handleChange(event) {
         const newValue = event.target.value;
         setInputText(newValue);
@@ -17,22 +17,33 @@ function App() {
         setInputText("");
     }
 
+    function deleteItem(id) {
+        setItems(prevItems => {
+            return prevItems.filter((item, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div className="container">
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
             <div className="form">
-                <input onChange={handleChange} type="text" value={inputText} />
+                <input onChange={handleChange} type="text" value={inputText} placeholder="what to do ?.." />
                 <button onClick={addItem}>
                     <span>Add</span>
                 </button>
             </div>
             <div>
                 <ul>
-                    {items.map(todoItem => (
+                    {items.map((todoItem, index) => (
                         <ToDoItem
+                            key={index}
+                            id={index}
                             text={todoItem}
+                            onPressed={deleteItem}
                         />
                     ))}
                 </ul>
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import deletelogo from './media/delete.png';
 
 function ToDoItem(props) {
     const [isDone, setIsDone] = useState(false);
@@ -10,12 +11,24 @@ function ToDoItem(props) {
     }
 
     return (
-        <div onClick={handleClick}>
-            <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        <div className="item-container">
+
+            <li style={{ textDecoration: isDone ? "line-through" : "none", color: isDone ? "#0000004c" : "black" }}
+                onClick={handleClick}
+            >
                 {props.text}
+
             </li>
+
+            <img src={deletelogo} alt="delete_logo"
+                onClick={() => {
+                    props.onPressed(props.id);
+                }}
+            />
+
         </div>
     );
 }
+
 
 export default ToDoItem;
